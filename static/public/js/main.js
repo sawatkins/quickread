c0f35db97998705de36bcc8d979a39ab637566d1
sawatkins <siawatkins@gmail.com>
2023-08-13 21:28:22 -0700

    filename after browse files

--- static/public/js/main.js
+++ static/public/js/main.js
@@ -1,12 +1,23 @@
-window.onload = function() {
-    
+window.onload = function () {
+
     document.getElementById("form-upload-doc").addEventListener("submit", function (e) {
         e.preventDefault();
-    
+
         // Handle form submission here, e.g., upload the file to a server
         console.log("Form submitted");
     });
-    
+
     console.log("loaded main.js");
 
+}
+
+function displayFileName() {
+    const fileInput = document.getElementById('input-upload-doc');
+    const fileName = document.getElementById('upload-doc-filename');
+
+    if (fileInput.files.length > 0) {
+        fileName.textContent = "File: " + fileInput.files[0].name;
+    } else {
+        fileName.textContent = '';
+    }
 }
